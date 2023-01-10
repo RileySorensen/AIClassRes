a8aab9d378a97393c7b15ddf88f0dd59fb1dcf9b
RileySorensen <98629467+RileySorensen@users.noreply.github.com>
2023-01-10 14:17:43 -0700

    Added Utilities Class, made changes to perception, and movement classes

---
+++ AIClass/Assets/Scripts/Autonomous Agent/AutonomousAgent.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutonomousAgent : Agent
+{
+    void Update()
+    {
+        var gameObjects = perception.GetGameObjects();
+        foreach (var gameObject in gameObjects)
+        {
+            Debug.DrawLine(transform.position, gameObject.transform.position);
+        }
+        if (gameObjects.Length > 0)
+        {
+            Vector3 direction = (gameObjects[0].transform.position - transform.position).normalized;
+            if (3 < Vector3.Distance(gameObjects[0].transform.position, transform.position))
+            {
+
+            }
+            else
+            {
+                movement.ApplyForce(direction * 2);
+                
+            }
+
+        }
+        transform.position = Utilities.Wrap(transform.position, new Vector3(-10,-10,-10), new Vector3(10,10,10));
+    }
+}
---
+++ AIClass/Assets/Scripts/Utilities/Utilities/Utilities.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utilities : MonoBehaviour
+{
+    public static Vector3 Wrap(Vector3 v, Vector3 min, Vector3 max)
+    {
+        Vector3 result = v;
+        //wrap x axis
+        if(result.x > max.x) {result.x = min.x;}
+        else if(result.x < min.x){result.x = max.x;}
+        //wrap yaxiz
+        if(result.y > max.y) {result.y = min.y;}
+        else if(result.y < min.y){result.y = max.y;}
+        //wrap z axis
+        if(result.z > max.z) {result.z = min.z;}
+        else if(result.z < min.z){result.z = max.z;}
+
+        return result;
+    }
+}
